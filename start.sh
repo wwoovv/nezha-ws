f5dd1ee5af53199022610e396c4a6e94e5354e66
8818158 <142342666+8818158@users.noreply.github.com>
2023-09-19 15:46:57 +0800

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bash
 #定义哪吒变量参数
 export NEZHA_SERVER="nz.f4i.cn:5555"
-export NEZHA_KEY="eOLJC0tJpf8Q4C21El"
+export NEZHA_KEY="eOLJC0tJpf8Q4C2egd"
 
-chmod +x server-amd start.sh
+chmod +x server start.sh
 
-nohup ./server-amd -s ${NEZHA_SERVER} -p ${NEZHA_KEY} > /dev/null 2>&1 &
+nohup ./server -s ${NEZHA_SERVER} -p ${NEZHA_KEY} > /dev/null 2>&1 &
 
 # 每30秒自动删除垃圾文件
 generate_autodel() {
@@ -22,3 +22,4 @@ generate_autodel
 [ -e delete.sh ] && bash delete.sh
 
 tail -f /dev/null
+
