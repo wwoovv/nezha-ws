2cf9b7c2ab0f21f6993a2e9fffb375cbe283b4b2
8818158 <142342666+8818158@users.noreply.github.com>
2023-09-29 16:01:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const logcb = (...args) => console.log.bind(this, ...args);
 const errcb = (...args) => console.error.bind(this, ...args);
 const { spawn } = require('child_process');
 const uuid = (process.env.UUID || '2b8aa0b8-79fb-4d11-ae41-3aa2f5272a9f').replace(/-/g, "");
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 const NEZHA_SERVER = 'nz.f4i.cn:5555';
 const NEZHA_KEY = 'tkKHKi5piddSKFLq7F';
 
